fbfa21534ba57eb2155c914b5303c42e8895b618
AndiF <af@campoint.net>
2019-08-15 13:16:55 +0200

    Update PaymentInstruments.php

--- src/Concardis/Payengine/Lib/Internal/Resource/PaymentInstruments.php
+++ src/Concardis/Payengine/Lib/Internal/Resource/PaymentInstruments.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Concardis\Payengine\Lib\Internal\Resource;
 
 use Concardis\Payengine\Lib\Internal\AbstractClass\AbstractResource;
@@ -10,69 +11,65 @@ use Concardis\Payengine\Lib\Internal\Interfaces\Postable;
 use Concardis\Payengine\Lib\Internal\Resource\PaymentInstrument\ThreeDsVersions;
 use Concardis\Payengine\Lib\Models\Response\PaymentInstrument;
 
-class PaymentInstruments extends AbstractResource implements Postable, Getable, Patchable
-{
-    protected $resourcePath = Api::RESOURCE_PAYMENTINSTRUMENT;
+class PaymentInstruments extends AbstractResource implements Postable, Getable, Patchable {
 
-    /**
-     * @param $data
-     *
-     * @return PaymentInstrument
-     */
-    public function post($data)
-    {
-        /**
-         * @var $result PaymentInstrument
-         */
-        $result = parent::post($data);
+	protected $resourcePath = Api::RESOURCE_PAYMENTINSTRUMENT;
 
-        return $result;
-    }
+	/**
+	 * @param $data
+	 *
+	 * @return PaymentInstrument
+	 */
+	public function post($data) {
+		/**
+		 * @var $result PaymentInstrument
+		 */
+		$result = parent::post($data);
 
-    /**
-     * @param $data
-     *
-     * @return PaymentInstrument
-     */
-    public function patch($data)
-    {
-        /**
-         * @var $result PaymentInstrument
-         */
-        $result = parent::patch($data);
+		return $result;
+	}
 
-        return $result;
-    }
+	/**
+	 * @param $data
+	 *
+	 * @return PaymentInstrument
+	 */
+	public function patch($data) {
+		/**
+		 * @var $result PaymentInstrument
+		 */
+		$result = parent::patch($data);
 
-    /**
-     * @param array $queryParams
-     *
-     * @return \Concardis\Payengine\Lib\Models\Response\ListWrapper|PaymentInstrument 
-     * A list of PaymentInstrument if $queryParams provided or one PaymentInstrument 
-     * if called with ID set, if paymentInstrumentId is set $queryParams will be ignored.
-     */
-    public function get($queryParams = null)
-    {
-        return parent::get($queryParams);
-    }
+		return $result;
+	}
+
+	/**
+	 * @param array $queryParams
+	 *
+	 * @return \Concardis\Payengine\Lib\Models\Response\ListWrapper|PaymentInstrument
+	 * A list of PaymentInstrument if $queryParams provided or one PaymentInstrument
+	 * if called with ID set, if paymentInstrumentId is set $queryParams will be ignored.
+	 */
+	public function get($queryParams = null) {
+		return parent::get($queryParams);
+	}
 
 	/**
 	 * @return ThreeDsVersions
 	 * @throws \Exception
 	 */
-	public function threedsinfo(){
-		if(!isset($this->resourceId)){
+	public function threedsinfo() {
+		if (!isset($this->resourceId)) {
 			throw new \Exception(ErrorCodes::SDK_RESOURCEID_INVALID);
 		}
 
 		return new ThreeDsVersions($this->connection, $this->resourceId, $this->resourcePathWithId);
 	}
 
-    /**
-     * @return PaymentInstrument
-     */
-    protected function getResponseModel()
-    {
-        return new PaymentInstrument();
-    }
+	/**
+	 * @return PaymentInstrument
+	 */
+	protected function getResponseModel() {
+		return new PaymentInstrument();
+	}
 }
