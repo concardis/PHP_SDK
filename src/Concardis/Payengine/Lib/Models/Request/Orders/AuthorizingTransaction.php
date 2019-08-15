414b96640f1c1b5d72b0a38a543626f580e830d3
AndiF <af@campoint.net>
2019-08-15 13:19:06 +0200

    Update AuthorizingTransaction.php

--- src/Concardis/Payengine/Lib/Models/Request/Orders/AuthorizingTransaction.php
+++ src/Concardis/Payengine/Lib/Models/Request/Orders/AuthorizingTransaction.php
@@ -4,70 +4,100 @@ namespace Concardis\Payengine\Lib\Models\Request\Orders;
 
 use Concardis\Payengine\Lib\Models\Request\AbstractClass\AbstractOrder;
 
-class AuthorizingTransaction extends AbstractOrder
-{
+class AuthorizingTransaction extends AbstractOrder {
 
-    /**
-     * @var \Concardis\Payengine\Lib\Models\Request\Orders\Payment\Payment
-     */
-    private $payment;
+	/**
+	 * @var \Concardis\Payengine\Lib\Models\Request\Orders\Payment\Payment
+	 */
+	private $payment;
 
-    /**
-     * @var string
-     */
-    private $product;
+	/**
+	 * @var string
+	 */
+	private $product;
 
-    /**
-     * @var Async
-     */
-    private $async;
+	/**
+	 * @var Async
+	 */
+	private $async;
 
-    /**
-     * @return \Concardis\Payengine\Lib\Models\Request\Orders\Payment\Payment
-     */
-    public function getPayment()
-    {
-        return $this->payment;
-    }
+	/**
+	 * @var Meta
+	 */
+	private $meta;
 
-    /**
-     * @param \Concardis\Payengine\Lib\Models\Request\Orders\Payment\Payment $payment
-     */
-    public function setPayment($payment)
-    {
-        $this->payment = $payment;
-    }
+	/**
+	 * @var Risk
+	 */
+	private $risk;
 
-    /**
-     * @return string
-     */
-    public function getProduct()
-    {
-        return $this->product;
-    }
+	/**
+	 * @return \Concardis\Payengine\Lib\Models\Request\Orders\Payment\Payment
+	 */
+	public function getPayment() {
+		return $this->payment;
+	}
 
-    /**
-     * @param string $product
-     */
-    public function setProduct($product)
-    {
-        $this->product = $product;
-    }
+	/**
+	 * @param \Concardis\Payengine\Lib\Models\Request\Orders\Payment\Payment $payment
+	 */
+	public function setPayment($payment) {
+		$this->payment = $payment;
+	}
 
-    /**
-     * @return Async
-     */
-    public function getAsync()
-    {
-        return $this->async;
-    }
+	/**
+	 * @return string
+	 */
+	public function getProduct() {
+		return $this->product;
+	}
 
-    /**
-     * @param Async $async
-     */
-    public function setAsync($async)
-    {
-        $this->async = $async;
-    }
+	/**
+	 * @param string $product
+	 */
+	public function setProduct($product) {
+		$this->product = $product;
+	}
 
+	/**
+	 * @return Async
+	 */
+	public function getAsync() {
+		return $this->async;
+	}
+
+	/**
+	 * @param Async $async
+	 */
+	public function setAsync($async) {
+		$this->async = $async;
+	}
+
+	/**
+	 * @return Meta
+	 */
+	public function getMeta() {
+		return $this->meta;
+	}
+
+	/**
+	 * @param Meta $meta
+	 */
+	public function setMeta($meta) {
+		$this->meta = $meta;
+	}
+
+	/**
+	 * @return Risk
+	 */
+	public function getRisk() {
+		return $this->risk;
+	}
+
+	/**
+	 * @param Risk $risk
+	 */
+	public function setRisk($risk) {
+		$this->risk = $risk;
+	}
 }
